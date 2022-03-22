eb9228207b6d89528a0c7d0a877653231543d5e6
Debdeep Pal <deep64222@gmail.com>
2022-03-22 08:52:19 +0530

    Init

---
+++ src/main/java/dev/dtech/Main.java
@@ -0,0 +1,7 @@
+package dev.dtech;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
---
+++ src/main/java/dev/dtech/Util.java
@@ -0,0 +1,4 @@
+package dev.dtech;
+
+public class Util {
+}
